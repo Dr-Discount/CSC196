76b8e5fab3264d747517308a52db76e98a0032ed
Dr-Discount <trackerjack02@gmail.com>
2025-07-22 10:21:13 -0600

    hmmm

--- Source/Game/Game/Player.cpp
+++ Source/Game/Game/Player.cpp
@@ -10,7 +10,7 @@ void Player::Update(float dt) {
 	if (viper::GetEngine.GetInput().GetKeyDown(SDL_SCANCODE_A)) rotate -= 1;
 	if (viper::GetEngine.GetInput().GetKeyDown(SDL_SCANCODE_D)) rotate += 1;
 	m_transform.rotation += (rotate * rotationRate) * dt;
-
+	
 	float thrust = 0;
 	if (input->GetKeyDown(SDL_SCANCODE_W)) thrust = 1;
 	if (input->GetKeyDown(SDL_SCANCODE_S)) thrust = 1;
